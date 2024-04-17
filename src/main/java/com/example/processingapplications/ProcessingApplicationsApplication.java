3a11d2cbf8de4238f3fe494f698baba42bc67808
AABelimov <belimov98@gmail.com>
2024-04-17 15:40:31 +0300

    Initial commit

---
+++ src/main/java/com/example/processingapplications/ProcessingApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.processingapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingApplicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessingApplicationsApplication.class, args);
+    }
+
+}
